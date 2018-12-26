3d1cf71a48e0bd80c6f3741b6dab47044ba70507
Sai Manoj Kumar Yadlapati <sai-manoj-kumar@users.noreply.github.com>
2018-12-27 03:11:23 +0530

    Rename sample config files

--- deploy10scenarios.ps1
+++
@@ -1,5 +0,0 @@
-.\deploy.ps1 -subscriptionId 1496a758-b2ff-43ef-b738-8e9eb5161a86 -resourceGroupName saihcrudtest1 -Verbose
-.\deploy.ps1 -subscriptionId 1496a758-b2ff-43ef-b738-8e9eb5161a86 -resourceGroupName saihcrudtest2 -Verbose
-.\deploy.ps1 -subscriptionId 1496a758-b2ff-43ef-b738-8e9eb5161a86 -resourceGroupName saihcrudtest3 -Verbose
-.\deploy.ps1 -subscriptionId 1496a758-b2ff-43ef-b738-8e9eb5161a86 -resourceGroupName saihcrudtest4 -Verbose
-.\deploy.ps1 -subscriptionId 1496a758-b2ff-43ef-b738-8e9eb5161a86 -resourceGroupName saihcrudtest5 -Verbose
