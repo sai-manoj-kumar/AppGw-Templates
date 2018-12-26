e90d3ec3ed370cb819bb78cdacf4c7a981622fad
Sai Manoj Kumar Yadlapati <saiyad@microsoft.com>
2018-12-27 02:17:12 +0530

    Add CreatedBy,ExpiresBy tags to the RG

--- with-conditions/deploy.ps1
+++ with-conditions/deploy.ps1
@@ -25,27 +25,39 @@
 #>
 
 param(
- [Parameter(Mandatory=$True)]
- [string]
- $subscriptionId,
+    [Parameter]
+    [string]
+    $subscriptionId,
 
- [Parameter(Mandatory=$True)]
- [string]
- $resourceGroupName,
+    [Parameter(Mandatory = $True)]
+    [string]
+    $resourceGroupName,
 
- [string]
- $resourceGroupLocation = "centraluseuap",
+    [string]
+    $resourceGroupLocation,
 
- [Parameter(Mandatory=$True)]
- [string]
- $deploymentName,
+    [string]
+    $deploymentName = "deployment1",
 
- [string]
- $templateFilePath = "template.json",
+    [string]
+    $templateFilePath = "template.json",
 
- [string]
- $parametersFilePath = "parameters.json"
+    [string]
+    $parametersFilePath = "parameters.json",
+
+    [string]
+    [ValidateSet("CentralUSSlice", "InternalSubscription1", "OtherSubscription")]
+    $Subscription = "OtherSubscription"
 )
+
+if ($Subscription -eq "CentralUSSlice") {
+    $resourceGroupLocation = "centraluseuap"
+    $subscriptionId = "1496a758-b2ff-43ef-b738-8e9eb5161a86"
+}
+elseif ($Subscription -eq "InternalSubscription1") {
+    $resourceGroupLocation = "eastus"
+    $subscriptionId = "f7e1a56e-347b-4103-87c7-e775a3e11ac5"
+}
 
 <#
 .SYNOPSIS
@@ -75,33 +87,34 @@ Write-Host "Selecting subscription '$subscriptionId'";
 Select-AzureRmSubscription -SubscriptionID $subscriptionId;
 
 # Register RPs
-$resourceProviders = @("microsoft.network","microsoft.compute","microsoft.storage","microsoft.devtestlab");
-if($resourceProviders.length) {
+$resourceProviders = @("microsoft.network", "microsoft.compute", "microsoft.storage", "microsoft.devtestlab");
+if ($resourceProviders.length) {
     Write-Host "Registering resource providers"
-    foreach($resourceProvider in $resourceProviders) {
-#        RegisterRP($resourceProvider);
+    foreach ($resourceProvider in $resourceProviders) {
+        #        RegisterRP($resourceProvider);
     }
 }
 
 #Create or check for existing resource group
 $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
-if(!$resourceGroup)
-{
+if (!$resourceGroup) {
     Write-Host "Resource group '$resourceGroupName' does not exist. To create a new resource group, please enter a location.";
-    if(!$resourceGroupLocation) {
+    if (!$resourceGroupLocation) {
         $resourceGroupLocation = Read-Host "resourceGroupLocation";
     }
     Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
-    New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
+    $now = Get-Date
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Tag @{'CreatedBy' = $env:UserName; 'ExpiresBy' = $now.ToUniversalTime().AddDays(3).GetDateTimeFormats()[9]}
 }
-else{
+else {
     Write-Host "Using existing resource group '$resourceGroupName'";
 }
 
 # Start the deployment
 Write-Host "Starting deployment...";
-if(Test-Path $parametersFilePath) {
+if (Test-Path $parametersFilePath) {
     $lasterror = New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath;
-} else {
+}
+else {
     New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath
 }
---
+++ wo-conditions/deploy.ps1
@@ -0,0 +1,128 @@
+<#
+ .SYNOPSIS
+    Deploys a template to Azure
+
+ .DESCRIPTION
+    Deploys an Azure Resource Manager template
+
+ .PARAMETER subscriptionId
+    The subscription id where the template will be deployed.
+
+ .PARAMETER resourceGroupName
+    The resource group where the template will be deployed. Can be the name of an existing or a new resource group.
+
+ .PARAMETER resourceGroupLocation
+    Optional, a resource group location. If specified, will try to create a new resource group in this location. If not specified, assumes resource group is existing.
+
+ .PARAMETER deploymentName
+    The deployment name.
+
+ .PARAMETER templateFilePath
+    Optional, path to the template file. Defaults to template.json.
+
+ .PARAMETER parametersFilePath
+    Optional, path to the parameters file. Defaults to parameters.json. If file is not found, will prompt for parameter values based on template.
+#>
+
+param(
+    [string]
+    $subscriptionId,
+
+    [Parameter(Mandatory = $True)]
+    [string]
+    $resourceGroupName,
+
+    [string]
+    $resourceGroupLocation,
+
+    [string]
+    $deploymentName = "deployment1",
+
+    [string]
+    $templateFilePath = "template.json",
+
+    [string]
+    $parametersFilePath = "parameters.json",
+
+    [string]
+    [ValidateSet("CentralUSSlice", "InternalSubscription1", "OtherSubscription")]
+    $Subscription = "OtherSubscription"
+)
+
+if ($Subscription -eq "CentralUSSlice") {
+    $resourceGroupLocation = "centraluseuap"
+    $subscriptionId = "1496a758-b2ff-43ef-b738-8e9eb5161a86"
+}
+elseif ($Subscription -eq "InternalSubscription1") {
+    $resourceGroupLocation = "eastus"
+    $subscriptionId = "f7e1a56e-347b-4103-87c7-e775a3e11ac5"
+}
+
+<#
+.SYNOPSIS
+    Registers RPs
+#>
+Function RegisterRP {
+    Param(
+        [string]$ResourceProviderNamespace
+    )
+
+    Write-Host "Registering resource provider '$ResourceProviderNamespace'";
+    Register-AzureRmResourceProvider -ProviderNamespace $ResourceProviderNamespace;
+}
+
+#******************************************************************************
+# Script body
+# Execution begins here
+#******************************************************************************
+$ErrorActionPreference = "Stop"
+
+function Login {
+    if ([string]::IsNullOrEmpty($(Get-AzureRmContext).Account)) {
+        Write-Host "Not Logged in yet";
+        Login-AzureRmAccount
+    }
+    else {
+        Write-Host "Already Logged in";
+    }
+}
+
+# Login if not already logged in
+Login
+
+# select subscription
+Write-Host "Selecting subscription '$subscriptionId'";
+Select-AzureRmSubscription -SubscriptionID $subscriptionId;
+
+# Register RPs
+$resourceProviders = @("microsoft.network", "microsoft.compute", "microsoft.storage", "microsoft.devtestlab");
+if ($resourceProviders.length) {
+    Write-Host "Registering resource providers"
+    foreach ($resourceProvider in $resourceProviders) {
+        #        RegisterRP($resourceProvider);
+    }
+}
+
+#Create or check for existing resource group
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if (!$resourceGroup) {
+    Write-Host "Resource group '$resourceGroupName' does not exist. To create a new resource group, please enter a location.";
+    if (!$resourceGroupLocation) {
+        $resourceGroupLocation = Read-Host "resourceGroupLocation";
+    }
+    Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
+    $now = Get-Date
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Tag @{'CreatedBy' = $env:UserName; 'ExpiresBy' = $now.ToUniversalTime().AddDays(3).GetDateTimeFormats()[9]}
+}
+else {
+    Write-Host "Using existing resource group '$resourceGroupName'";
+}
+
+# Start the deployment
+Write-Host "Starting deployment...";
+if (Test-Path $parametersFilePath) {
+    $lasterror = New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath
+}
+else {
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath
+}
